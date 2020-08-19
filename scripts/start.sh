b832faade004cc2845bf2f27614502a12292756b
Phoenix <68136741+travelfoodblends@users.noreply.github.com>
2020-08-19 00:50:04 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 echo 'Starting my app'
-java -jar /home/ec2-user/server/target/ResourcingService-0.0.1-SNAPSHOT.jar
+nohup java -jar ResourcingService-0.0.1-SNAPSHOT.jar > resourcing-service.log
 exit 0
