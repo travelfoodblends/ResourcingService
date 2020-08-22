5480eda3f595055c2f0cce3027a6d5f73e5856c5
Phoenix <68136741+travelfoodblends@users.noreply.github.com>
2020-08-21 21:41:56 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 echo 'Starting my app'
-nohup java -jar /home/ec2-user/server/target/ResourcingService-0.0.1-SNAPSHOT.jar &
+nohup java -jar /home/ec2-user/server/target/ResourcingService-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
 echo 'app started'
 exit 0
