d16adf8d512cec1d10e4e36abf389ed75ec9a295
Phoenix <68136741+travelfoodblends@users.noreply.github.com>
2020-09-20 22:20:06 -0600

    Update start.sh

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo 'Starting my app'
-nohup java -jar /home/ec2-user/server/target/ResourcingService-0.0.1-SNAPSHOT.jar > /dev/null 2> /dev/null < /dev/null &
+nohup java -jar /home/ec2-user/server/target/ResourcingService-0.0.1-SNAPSHOT.jar > /var/log/app/serviceLog.txt 2>&1 &
+echo $! > /dev/my-service-logs/pid.file
 echo 'app started'
 exit 0
